442da4037cdb34b8e76620180539421caabd2d63
cuongtech47 <cuongtech47@gmail.com>
2022-07-05 02:50:18 +0700

    fix ui dashboard

--- server/controllers/CategoryController.js
+++ server/controllers/CategoryController.js
@@ -1,11 +1,62 @@
-
+const Admin = require("../models/Admin");
+const jwt = require("jsonwebtoken");
 
 class CategoryController  {
     addCategory( req , res){
-        res.render("admin/add_category",{title: "Them Danh Muc San Pham"})
+        
+        // res.render("admin/add_category")
+        const token = req.cookies.jwt
+        if (token) {
+            jwt.verify(
+              token,
+              process.env.ACCESS_TOKEN_SECRET,
+              (err, decodedAdmin) => {
+                if (err) {
+                  console.log(err);
+                  next(err);
+                } else {
+                  console.log('decodedAdmin',decodedAdmin)
+                  Admin.findById(decodedAdmin.adminId, (err, adminData) => {
+                    const admin_name = adminData.admin_name;
+                    res.render("admin/add_category",{ admin_name})
+                  });
+                }
+              }
+            );
+          } else {
+            const admin_name = "Unknown";
+            res.render("admin/add_category" ,{ admin_name});
+          }
     }
     allCategory( req , res) {
-        res.render("admin/all_category",{title: "Liet Ke Danh Muc San Pham"})
+       
+        const token = req.cookies.jwt
+        if (token) {
+            jwt.verify(
+              token,
+              process.env.ACCESS_TOKEN_SECRET,
+              (err, decodedAdmin) => {
+                if (err) {
+                  console.log(err);
+                  next(err);
+                } else {
+                  console.log('decodedAdmin',decodedAdmin)
+                  Admin.findById(decodedAdmin.adminId, (err, adminData) => {
+                    const admin_name = adminData.admin_name;
+                    res.render("admin/all_category",{ admin_name})
+                  });
+                }
+              }
+            );
+          } else {
+            const admin_name = "Unknown";
+            res.render("admin/all_category" ,{ admin_name});
+          }
+    }
+    postAddCategory(req , res) {
+        const {category_name ,category_desc , category_status ,category_slug  } = req.body
+        console.log(category_name ,category_desc , category_status ,category_slug)
+
     }
 }
 
--- server/routes/category.js
+++ server/routes/category.js
@@ -3,9 +3,12 @@ const router = express.Router()
 
 
 const categoryController = require("../controllers/CategoryController")
+const isAuth = require("../middlewares/auth")
 
-router.get("/add-category",categoryController.addCategory)
-router.get("/all-category",categoryController.allCategory)
+router.get("/add-category",isAuth.reqAuthentication,categoryController.addCategory)
+router.get("/all-category",isAuth.reqAuthentication,categoryController.allCategory)
+
+router.post("/add-category",categoryController.postAddCategory)
 
 
 module.exports = router
